d2f500dfca48a81f94af6b62c07da6e0e748478d
shrujapatel <102239579+shrujapatel@users.noreply.github.com>
2022-03-24 15:16:29 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,7 @@
+var a=10;
+if(a==10)
+{
+    let a=15;
+    console.log(`Value of a = ${a}`);
+}
+console.log(`Value of a = ${a}`);
---
+++ templateLiteral.js
@@ -0,0 +1,9 @@
+var a=10;
+console.log("----String Literal");
+console.log("Value of a = "+a);
+console.log("Apple Banana Cat");
+console.log(`----Template Literal`);
+console.log(`Value of a = ${a}`);
+console.log(`Apple
+     Banana 
+        Cat`);
